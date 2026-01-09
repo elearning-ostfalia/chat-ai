6865cb99976507d838f9c1585a0a5efa3ca712bd
Karin Borm <k.borm@ostfalia.de>
2026-01-09 12:04:17 +0100

    customize

--- front/src/components/Sidebar/SidebarContent.tsx
+++ front/src/components/Sidebar/SidebarContent.tsx
@@ -14,6 +14,7 @@ import { useConversationList } from "../../db";
 import { useModal } from "../../modals/ModalContext";
 
 import Logo from "../../assets/logos/chat_ai.svg";
+import ostfalia_logo from "../../assets/logos/ostfalia.svg";
 
 import { toggleSidebar } from "../../Redux/reducers/interfaceSettingsSlice";
 
@@ -183,6 +184,16 @@ export default function SidebarContent({
         WebkitTapHighlightColor: "transparent",
       }}
     >
+      {/* Ostfalia logo */}
+      <div>
+          <div className="flex items-center justify-between">
+                <img
+                    className="h-10"
+                    src={ostfalia_logo}
+                    alt="Ostfalia Logo"
+                />
+          </div>
+      </div>
       {/* Header with close button */}
       <div className="flex items-center justify-between">
         <AiServicesMenu />
--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       colors: {
         primary: "#B4388D",
         secondary: "#695EA2",
-        tertiary: "#009EE0",
+        tertiary: "#2330b0", // Ostfalia "#009EE0",
         dark_hover: "#333333",
         light_hover: "#efeff1",
         border_dark: "#333333",
